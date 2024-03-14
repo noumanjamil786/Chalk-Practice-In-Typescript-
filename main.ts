97c6132183a30366abf0b2de23aca75b69797cef
Nouman Jamil <noumanjamil113@gmail.com>
2024-03-14 16:11:40 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Hello,") + chalk.green("world"));
+
+console.log(chalk.red("Hello am a New,") + chalk.bold.yellow("Programmer"));
+
+console.log(chalk.yellow.bold("Hello Nouman Would you like to learn Typscript?")); 
+
+console.log(chalk.red.underline("What is Your Name ?"));
